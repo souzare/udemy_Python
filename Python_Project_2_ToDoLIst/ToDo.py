607a8df7b9374e76f26e5d401941cf5539931225
Renan <renan.de.souza@accenture.com>
2025-01-20 13:27:23 -0300

    Aula 20

--- Python_Project_2_ToDoLIst/ToDo.py
+++ Python_Project_2_ToDoLIst/ToDo.py
@@ -1,9 +1,8 @@
 user_prompt = "Enter a ToDo:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
-print (todos)
-
-print(type(user_prompt))
+while True:
+    todo = input(user_prompt)
+    todos = [todo]
+    print(todos)
+    
+    
