21cbf7953cd18f57312dc9dfbe0521d8ffd14f5a
Renan Souza <renansouza.ds@gmail.com>
2025-08-14 08:00:58 -0300

    aula 41

--- Python_Project_2_ToDoLIst/ToDo.py
+++ Python_Project_2_ToDoLIst/ToDo.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or extit: ")
+    user_action = input("Type add, show, edit or extit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,9 +10,16 @@ while True:
             todos.append(todo)
         case 'show':
                 for item in todos:
+                        item = item.title()
                         print(item)
+        case 'edit':
+                number = int(input("Number of the todo to edit:"))
+                number = number - 1 ##aqui é para refletir exatamente o indici partido de 1
+                new_todo = input("Enter a new todo:") 
+                todos[number] = new_todo
         case 'exit':
                 break
+
 
 print("Bye!")
     
