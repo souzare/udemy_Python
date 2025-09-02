da96f3c20ddee32405b096d3ffc74c990e046340
Renan Souza <renansouza.ds@gmail.com>
2025-09-02 08:34:42 -0300

    aula 51

--- Python_Project_2_ToDoLIst/ToDo.py
+++ Python_Project_2_ToDoLIst/ToDo.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or extit: ")
+    user_action = input("Type add, show, edit, complete or extit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,13 +10,16 @@ while True:
             todos.append(todo)
         case 'show':
                 for index, item in enumerate(todos):
-                        row = f"{index}-{item}"
+                        row = f"{index + 1}-{item}"
                         print(row)
         case 'edit':
                 number = int(input("Number of the todo to edit:"))
                 number = number - 1 ##aqui é para refletir exatamente o indici partido de 1
                 new_todo = input("Enter a new todo:") 
                 todos[number] = new_todo
+        case 'complete':
+                number = int(input("Number of the todo to complete:"))
+                todos.pop(number - 1)
         case 'exit':
                 break
 
