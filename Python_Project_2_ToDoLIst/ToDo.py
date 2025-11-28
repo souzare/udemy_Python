bd26e805477774661a42b0c2c7d5f13019cf5bbe
Renan <renansouza.ds@gmail.com>
2025-11-28 08:46:52 -0300

    Aula 60

--- Python_Project_2_ToDoLIst/ToDo.py
+++ Python_Project_2_ToDoLIst/ToDo.py
@@ -1,20 +1,24 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or extit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+            file = open("todos.txt", "r")
+            todos = file.readlines()
+
             todos.append(todo)
+            
+            file = open("todos.txt", "w") ##aqui abre o arquivo em modo de escrita
+            file.writelines(todos) ##aqui escreve a lista no arquivo
         case 'show':
                 for index, item in enumerate(todos):
                         row = f"{index + 1}-{item}"
                         print(row)
         case 'edit':
                 number = int(input("Number of the todo to edit:"))
-                number = number - 1 ##aqui é para refletir exatamente o indici partido de 1
+                number = number - 1 ##aqui é para refletir exatamente o indice partido de 1
                 new_todo = input("Enter a new todo:") 
                 todos[number] = new_todo
         case 'complete':
